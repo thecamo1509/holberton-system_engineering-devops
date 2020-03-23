a759dddd623f0df45bc43f8de53b7d50603bd1ad
Camilo Morales <camiloandres.1509@gmail.com>
2020-03-23 14:15:35 -0500

    Uploading the files

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,5 @@
+# This will install the puppet-lint
+package { 'puppet-lint':
+	ensure   => '2.1.1',
+	provider => 'gem',
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,5 @@
+# This manifest will terminate a process called killmenow
+exec { 'killmenow':
+	command  => 'pkill killmenow',
+	path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+}
