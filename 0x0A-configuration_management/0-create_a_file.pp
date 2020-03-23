071f5c8d1c81d828150e146310356c72b43bb442
Camilo Morales <camiloandres.1509@gmail.com>
2020-03-23 13:00:42 -0500

    Fixed the errors

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,7 @@
-file {'/etc/nsswitch.conf':
-  content  => 'I love Puppet',
-  group    => 'www-data',
-  mode     => '0744',
-  owner    => 'www-data',
+# This will create a file with initial config
+file {'/tmp/holberton':
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
 }
