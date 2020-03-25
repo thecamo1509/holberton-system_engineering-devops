ec552164eac52c7cc6a1a3104c33ebb5ff325838
Camilo Morales <camiloandres.1509@gmail.com>
2020-03-25 17:32:03 -0500

    Correcting the manifest

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #Manifest to install nginx
-exec { 'Nginx install'
-  command  => 'sudo apt -y update && sudo apt -y install nginx && echo "Holberton School" >> /var/www/html/index.html && direction= "/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" && sudo sed -i "35i $direction" /etc/nginx/sites-enabled/default && service nginx start',
-  provider => shell,
+exec {'Install nginx':
+command  => 'sudo apt update && sudo apt -y install nginx && echo "Holberton School" | sudo tee /var/www/html/index.html && redirect="\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;\n" && sudo sed -i "37i $redirect" /etc/nginx/sites-enabled/default && sudo service nginx restart',
+provider => shell,
 }
