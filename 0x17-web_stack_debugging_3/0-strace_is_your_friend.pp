0a5277597cf8331495f00584220bef1d5c778985
Camilo Morales <camiloandres.1509@gmail.com>
2020-05-20 10:54:03 -0500

    Correcting puppet lint syntax

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
-# This is the manifest to fix wordpress using Puppet
-exec { 'fixing http 500':
-    command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-    path    => ['bin]
+# fix it and then automate it using Puppet
+exec { 'fixing error 500':
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => ['/bin']
 }
